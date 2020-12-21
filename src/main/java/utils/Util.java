2b5f7db88710c4bf9cb18bcde455b331770f870a
polavskiran <36887402+polavskiran@users.noreply.github.com>
2020-12-21 11:12:05 +0530

    Update Util.java
    updated file

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,77 +1,165 @@
 package utils;
 
+package testUtils;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.codehaus.plexus.util.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Select;
 
-import com.aventstack.extentreports.Status;
+import pages.TestBase;
 
-import pages.BaseClass;
-
-public class Util extends BaseClass {
-
+public class Util extends TestBase {
+		
 	/*
-	 * 
+	 * @method launchBrowser
+	 * @argument String browser
+	 * @return void
 	 */
-	public void launchBrowser(String browserName) {
-		System.out.println("baseURL = " + baseURL);
-		switch (browserName.toLowerCase()) {
+	public void launchBrowser(String browser) {
+		switch (browser.toLowerCase()) {
+
 		case "chrome":
+			
 			ChromeOptions chOptions = new ChromeOptions();
 			chOptions.addArguments("--incognito");
-			chOptions.setAcceptInsecureCerts(true);
-			// chOptions.setCapability(CapabilityType.BROWSER_NAME, "Google Chrome");
-			chOptions.addArguments("start-maximized");
-
+			chOptions.addArguments("--start-maximized");
+			
 			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "\\src\\main\\resources\\drivers\\chromedriver.exe");
+					System.getProperty("user.dir") + "//src//test//resources//drivers//chromedriver.exe");
 			driver = new ChromeDriver(chOptions);
-			// driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.get(baseURL);
+			driver.get(baseUrl);
 			break;
 
 		case "firefox":
+
+			FirefoxOptions ffOptions = new FirefoxOptions();
+			ffOptions.setAcceptInsecureCerts(true);
+			ffOptions.addArguments("-private");
+
 			System.setProperty("webdriver.gecko.driver",
-					System.getProperty("user.dir") + "\\src\\main\\resources\\drivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			driver.manage().window().maximize();
+					System.getProperty("user.dir") + "//src//test//resources//drivers//geckodriver.exe");
+			driver = new FirefoxDriver(ffOptions);
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.get(baseURL);
+			driver.manage().window().maximize();
+			driver.get(baseUrl);
 			break;
-
+			
 		case "edge":
-			break;
-
-		default:
+			System.setProperty("webdriver.edge.driver",
+					System.getProperty("user.dir") + "//src//test//resources//drivers//msedgedriver.exe");
+			driver = new EdgeDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get(baseUrl);
 			break;
 		}
 	}
 
-	public WebElement getElement(By locator) {
-		//test.log(locator);
-		
-		if(isWebElementPresent(locator)) {
-			return driver.findElement(locator);
-		}
-		
+	/*
+	 * @method captureScreenShot
+	 * 
+	 * @return String
+	 */
+	public static void captureScreenShot() {
+		// String screenShotPath = null;
 		try {
-			throw new NoSuchElementException("Element Not Found : " + locator);
-		}catch(RuntimeException re) {
-			//test.log(Status.FAIL, re);
-			throw re;
+			File outputFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			String absolutePath = System.getProperty("user.dir") + "/screenShots/"
+					+ new SimpleDateFormat("dd_MM_yyyy_HHmmss").format(new Date()) + ".png";
+			// String basePath = absolutePath;
+			// screenShotPath = "../" + new File(basePath).toURI().relativize(new
+			// File(absolutePath).toURI()).getPath();
+			FileUtils.copyFile(outputFile, new File(absolutePath));
+		} catch (final Exception e) {
+
 		}
 	}
 
-	public boolean isWebElementPresent(By locator) {
-		boolean flag = driver.findElements(locator).size() >= 1;
+	/**
+	 * @method getWebElement to return WebElement based on locators
+	 * @param locator
+	 * Eg: 'id:SubmitButton'
+	 * @return WebElement
+	 */
+	public static WebElement getWebElement(String locator) {
+		WebElement element = null;
 
-		return flag;
+		String[] splitLocator = locator.split(":");
+		String locatorType = splitLocator[0];
+		String locatorValue = splitLocator[1];
+
+		try {
+			if (locatorType.toLowerCase().equals("id"))
+				element = driver.findElement(By.id(locatorValue));
+			else if (locatorType.toLowerCase().equals("name"))
+				element = driver.findElement(By.name(locatorValue));
+			else if ((locatorType.toLowerCase().equals("classname")) || (locatorType.toLowerCase().equals("class")))
+				element = driver.findElement(By.className(locatorValue));
+			else if ((locatorType.toLowerCase().equals("tagname")) || (locatorType.toLowerCase().equals("tag")))
+				element = driver.findElement(By.className(locatorValue));
+			else if ((locatorType.toLowerCase().equals("linktext")) || (locatorType.toLowerCase().equals("link")))
+				element = driver.findElement(By.linkText(locatorValue));
+			else if (locatorType.toLowerCase().equals("partiallinktext"))
+				element = driver.findElement(By.partialLinkText(locatorValue));
+			else if ((locatorType.toLowerCase().equals("cssselector")) || (locatorType.toLowerCase().equals("css")))
+				element = driver.findElement(By.cssSelector(locatorValue));
+			else if (locatorType.toLowerCase().equals("xpath"))
+				element = driver.findElement(By.xpath(locatorValue));
+			else
+				throw new Exception("Unknown locator type '" + locatorType + "'");
+		} catch (final Exception e) {
+
+		}
+
+		return element;
+	}
+
+	/*
+	 * @method isElementEnabled
+	 * 
+	 * @param String locator
+	 * 
+	 * @return boolean
+	 */
+	public boolean isElementEnabled(String locator) {
+		boolean enabledFlag = false;
+		try {
+			enabledFlag = getWebElement(locator).isEnabled();
+		} catch (Exception e) {
+
+		}
+
+		return enabledFlag;
+	}
+	
+	/*
+	 * @method SelectUsingIndex
+	 */
+	public void SelectUsingIndex(String locator, int index) {
+		Select select = new Select(getWebElement(locator));
+		select.selectByIndex(index);
+		// Create log
+	}
+	
+	/*
+	 * @method SelectUsingValue
+	 */	
+	public void SelectUsingValue(String locator, String value) {
+		Select select = new Select(getWebElement(locator));
+		select.selectByValue(value);
 	}
 }
